cf80d1e92cb9f2f2c34a2f7afa10277c4869c45f
Sankalp <104098061+sankalp-7@users.noreply.github.com>
2022-10-04 15:26:19 +0530

    Update settings.py

--- library/library/settings.py
+++ library/library/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^&uc(xxtaw6%r#+w1y!bick=g22$9*1ghta4ds24ll^8hg^fcd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 LOGIN_REDIRECT_URL = 'home'
 # Application definition
